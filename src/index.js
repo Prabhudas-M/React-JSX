d9c9e21ec1fef70301c33ebeb8c643d26b20897b
Prabhudas-M <60952658+Prabhudas-M@users.noreply.github.com>
2020-10-26 12:39:35 -0400

    changes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>Welcome Angallamma</h1>
+    <p>Welcome prabhudas</p>
+  </div>,
+  document.getElementById("root")
+);
